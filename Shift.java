732e6317fc3231fd9906029dc28d524f81f5d0da
snehalin <55582815+snehalin@users.noreply.github.com>
2020-09-08 13:11:55 +0530

    Add files via upload

---
+++ Shift.java
@@ -0,0 +1,16 @@
+
+public class Shift {
+
+	public static void main(String[] args) {
+		byte a=20;
+		System.out.println(a<<1);
+	      System.out.println(a<<2);
+	      System.out.println(a>>1);  
+	    int x=-5;
+	      System.out.println(x>>>2);
+	       x=-30;
+	  System.out.println(x>>>25); 		  
+	      
+	}
+
+}
